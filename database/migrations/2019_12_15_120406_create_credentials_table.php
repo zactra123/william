117fc5e278b62f143891d99bef151281c4425a15
AraratPapyan <papyanararat11@gmail.com>
2019-12-15 20:10:53 +0400

    migration credentials

---
+++ database/migrations/2019_12_15_120406_create_credentials_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCredentialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('credentials', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('ck_login');
+            $table->string('ck_password');
+            $table->string('ex_login');
+            $table->string('ex_password');
+            $table->string('tu_login');
+            $table->string('tu_password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('credentials');
+    }
+}
