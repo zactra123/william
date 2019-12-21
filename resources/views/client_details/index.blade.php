c1f6b9dcc9c62102761d6cc08a389afaed541ee4
Hayk Papyan <hayk803@gmail.com>
2019-12-21 14:30:13 +0400

    change index page

--- resources/views/client_details/index.blade.php
+++ resources/views/client_details/index.blade.php
@@ -2,14 +2,12 @@
 
 
 @section('content')
-    // #1a41ad
-
     <style>
 
 
-        .karma, .experian, .tuMembership, .tuOnline {
+        .tab-selector {
             background-color:  #FFFFFF;
-            border-color:#1a41ad;
+            /*border-color:#1a41ad;*/
             color: #1a41ad;
             font-size: large;
             display: -webkit-box;
@@ -22,6 +20,19 @@
             justify-content: center;
         }
 
+        .tab-selector:hover{
+            background-color: #0c71c3;
+            color:#FFFFFF;
+        }
+
+        .tab-selector.active {
+            background-color:  #0c71c3;
+            color:#FFFFFF;
+        }
+        .card-header{
+            padding: 0 !important;
+        }
+
 
         .btn-outline-color:not(:disabled):not(.disabled):active,
         .btn-outline-color:not(:disabled):not(.disabled).active,
@@ -32,13 +43,13 @@
         }
 
 
-
-        .nameUpload{
-            font-family: "Times New Roman";
-            font-size: large;
-            background-color:  #0c71c3;
-            color: #FFFFFF;
+        .pdf-upload{
+            display: none
         }
+        .pdf-upload.active{
+            display: block;
+        }
+
         .btn-uploadBtn{
             background-color:  #0c71c3;
             color: #FFFFFF;
@@ -51,131 +62,73 @@
     <div class="page-content">
         <div class="fullwidth-block">
             <div class="card" style="text-align: center">
-                <h1> You can upload you credit report choose  one of the options </h1> <br> <br>
+                <h1> You can upload you credit report choose  one of the options </h1>
 
-
-                        <div class="btn-group btn-group-toggle" data-toggle="buttons">
-
-                                <button type="button" class="btn btn-outline-primary karma" id="karma">Credit Karma</button>
-
-
-                                <button type="button" class="btn btn-outline-primary experian" id="experian">Experian</button>
-
-
-                                <button type="button" class="btn  btn-outline-primary tuMembership" id="tuMembership">TransUnion CREDIT MONITORING</button>
-
-                                <button type="button" class="btn btn-outline-color  tuOnline" id="tuOnline"> TransUnion Online Dispute </button>
-
-                        </div>
-
-                <br><br>
             </div>
-
-            <br>
-
-
             <div class="row justify-content-center"  >
                 <div class="col-md-8">
-                    <div class="card karmaUpload" style="display: none">
-                        <div class="card-header font nameUpload">
-                            Upload credit karma report
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+
+                                <button type="button" class="btn tab-selector active" data-target="karma-tab" id="karma">Credit Karma</button>
+
+
+                                <button type="button" class="btn tab-selector" data-target="experian-tab" id="experian">Experian</button>
+
+
+                                <button type="button" class="btn tab-selector" data-target="tu-tab" id="tuMembership">TransUnion CREDIT MONITORING</button>
+
+                                <button type="button" class="btn tab-selector" data-target="tu-online-tab" id="tuOnline"> TransUnion Online Dispute </button>
+
+                            </div>
                         </div>
 
                         <div class="card-body">
                             {!! Form::open(['route'=>['client.uploadPdf'],'method' => 'POST','files' => 'true','enctype'=>'multipart/form-data', 'class' => 'm-form m-form--label-align-right']) !!}
                             @csrf
+                                <div class="pdf-upload karma-tab active">
 
-                            <div class="form-group files">
+                                    <div class="form-group files">
 
-                                <input type="file" name="credit_report_credit_karma" class="form-control" multiple="">
-                            </div>
+                                        <input type="file" name="credit_report[credit_karma]" class="form-control" multiple="">
+                                    </div>
+                                </div>
 
-                            <div class="form-group row mb-0 font">
-                                <div class="col-md-8 offset-md-5">
-                                    <button type="submit" class="btn btn-uploadBtn">
-                                        Upload
-                                    </button>
+                                <div class="pdf-upload experian-tab">
 
+                                    <div class="form-group files">
+                                        <label>Upload Your File </label>
+                                        <input type="file" name="credit_report[experian]" class="form-control" multiple="">
+                                    </div>
+                                </div>
+                                <div class="pdf-upload tu-tab">
+
+                                    <div class="form-group files">
+                                        <label>Upload Your File </label>
+                                        <input type="file" name="credit_report[tu][client_details]" class="form-control" multiple="">
+                                    </div>
+                                    <div class="form-group files">
+                                        <label>Upload Your File </label>
+                                        <input type="file" name="credit_report[tu][payment_history]" class="form-control" multiple="">
+                                    </div>
+                                </div>
+                                <div class="pdf-upload tu-online-tab">
+                                    <div class="form-group files">
+                                        <label>Upload Your File </label>
+                                        <input type="file" name="credit_report[tu_online]" class="form-control" multiple="">
+                                    </div>
 
                                 </div>
-                            </div>
-                            {!! Form::close() !!}
-                        </div>
-                    </div>
-                    <div class="card experianUpload" style="display: none">
-                        <div class="card-header font nameUpload"> Upload Expiran report </div>
-
-                        <div class="card-body">
-                            {!! Form::open(['route'=>['client.uploadPdf'],'method' => 'POST','files' => 'true','enctype'=>'multipart/form-data', 'class' => 'm-form m-form--label-align-right']) !!}
-                            @csrf
-
-                            <div class="form-group files">
-                                <label>Upload Your File </label>
-                                <input type="file" name="credit_report_expiran" class="form-control" multiple="">
-                            </div>
-
-                            <div class="form-group row mb-0 font">
-                                <div class="col-md-8 offset-md-5">
-                                    <button type="submit" class="btn btn-uploadBtn">
-                                        Upload
-                                    </button>
+                                <div class="form-group row mb-0 font">
+                                    <div class="col-md-8 offset-md-5">
+                                        <button type="submit" class="btn btn-uploadBtn">
+                                            Upload
+                                        </button>
 
 
+                                    </div>
                                 </div>
-                            </div>
-                            {!! Form::close() !!}
-                        </div>
-                    </div>
-                    <div class="card tuOnlineDisputeUpload" style="display: none">
-                        <div class="card-header font nameUpload"> Upload TransUnion CREDIT MONITORING </div>
-
-                        <div class="card-body">
-                            {!! Form::open(['route'=>['client.uploadPdf'],'method' => 'POST','files' => 'true','enctype'=>'multipart/form-data', 'class' => 'm-form m-form--label-align-right']) !!}
-                            @csrf
-
-                            <div class="form-group files">
-                                <label>Upload Your File </label>
-                                <input type="file" name="credit_report_trans_client_details" class="form-control" multiple="">
-                            </div>
-                            <div class="form-group files">
-                                <label>Upload Your File </label>
-                                <input type="file" name="credit_report_payment_history" class="form-control" multiple="">
-                            </div>
-
-                            <div class="form-group row mb-0 font">
-                                <div class="col-md-8 offset-md-5">
-                                    <button type="submit" class="btn btn-uploadBtn">
-                                        Upload
-                                    </button>
-
-
-                                </div>
-                            </div>
-                            {!! Form::close() !!}
-                        </div>
-                    </div>
-                    <div class="card tuMembershipUpload" style="display: none">
-                        <div class="card-header font nameUpload"> Upload TransUnion Online Dispute Service </div>
-
-                        <div class="card-body">
-                            {!! Form::open(['route'=>['client.uploadPdf'],'method' => 'POST','files' => 'true','enctype'=>'multipart/form-data', 'class' => 'm-form m-form--label-align-right']) !!}
-                            @csrf
-
-                            <div class="form-group files">
-                                <label>Upload Your File </label>
-                                <input type="file" name="credit_report_trans_union_online" class="form-control" multiple="">
-                            </div>
-
-
-                            <div class="form-group row mb-0 font">
-                                <div class="col-md-8 offset-md-5">
-                                    <button type="submit" class="btn btn-uploadBtn">
-                                        Upload
-                                    </button>
-
-
-                                </div>
-                            </div>
                             {!! Form::close() !!}
                         </div>
                     </div>
@@ -192,62 +145,69 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
     $(document).ready(function () {
-        $('INPUT[type="file"]').change(function () {
-            var ext = this.value.match(/\.(.+)$/)[1];
-            console.log(ext);
-            switch (ext) {
-                case 'pdf':
-                case 'PDF':
-                    $('#uploadButton').attr('disabled', false);
-                    break;
-                default:
-                    alert('This is not an allowed file type.');
-                    this.value = '';
-            }
-        });
+        $('.tab-selector').click(function(){
+            var tab = $(this).attr("data-target");
 
+            $('.tab-selector').removeClass("active");
+            $('.pdf-upload').removeClass("active");
+            $("." + tab).addClass("active")
+        })
 
-        $(".karma").on("click",function () {
-            console.log('jhg');
-
-
-
-
-            $(".karmaUpload").css('display','block');
-            $(".experianUpload").css('display','none');
-            $(".tuOnlineDisputeUpload").css('display','none');
-            $(".tuMembershipUpload").css('display','none');
-
-        });
-
-        $(".experian").on("click",function () {
-
-            $(".karmaUpload").css('display','none');
-            $(".experianUpload").css('display','block');
-            $(".tuOnlineDisputeUpload").css('display','none');
-            $(".tuMembershipUpload").css('display','none');
-
-        });
-
-        $(".tuMembership").on("click",function () {
-
-            $(".karmaUpload").css('display','none');
-            $(".experianUpload").css('display','none');
-            $(".tuOnlineDisputeUpload").css('display','block');
-            $(".tuMembershipUpload").css('display','none');
-
-        });
-
-        $(".tuOnline").on("click",function () {
-
-            $(".karmaUpload").css('display','none');
-            $(".experianUpload").css('display','none');
-            $(".tuOnlineDisputeUpload").css('display','none');
-            $(".tuMembershipUpload").css('display','block');
-
-
-        });
+    //     $('INPUT[type="file"]').change(function () {
+    //         var ext = this.value.match(/\.(.+)$/)[1];
+    //         console.log(ext);
+    //         switch (ext) {
+    //             case 'pdf':
+    //             case 'PDF':
+    //                 $('#uploadButton').attr('disabled', false);
+    //                 break;
+    //             default:
+    //                 alert('This is not an allowed file type.');
+    //                 this.value = '';
+    //         }
+    //     });
+    //
+    //
+    //     $(".karma").on("click",function () {
+    //         console.log('jhg');
+    //
+    //
+    //
+    //
+    //         $(".karmaUpload").css('display','block');
+    //         $(".experianUpload").css('display','none');
+    //         $(".tuOnlineDisputeUpload").css('display','none');
+    //         $(".tuMembershipUpload").css('display','none');
+    //
+    //     });
+    //
+    //     $(".experian").on("click",function () {
+    //
+    //         $(".karmaUpload").css('display','none');
+    //         $(".experianUpload").css('display','block');
+    //         $(".tuOnlineDisputeUpload").css('display','none');
+    //         $(".tuMembershipUpload").css('display','none');
+    //
+    //     });
+    //
+    //     $(".tuMembership").on("click",function () {
+    //
+    //         $(".karmaUpload").css('display','none');
+    //         $(".experianUpload").css('display','none');
+    //         $(".tuOnlineDisputeUpload").css('display','block');
+    //         $(".tuMembershipUpload").css('display','none');
+    //
+    //     });
+    //
+    //     $(".tuOnline").on("click",function () {
+    //
+    //         $(".karmaUpload").css('display','none');
+    //         $(".experianUpload").css('display','none');
+    //         $(".tuOnlineDisputeUpload").css('display','none');
+    //         $(".tuMembershipUpload").css('display','block');
+    //
+    //
+    //     });
     })
-
 
 </script>
--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -60,7 +60,7 @@
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 
                                         <a  href="{{route('client.details.edit',Auth::user()->id)}}">Edit your detail</a>
-                                        <a  href="{{route('client.credentials')}}"> Credentials/Update</a>
+                                        <a  href="{{route('client.credentials')}}"> Credentials</a>
                                         <a class="dropdown-item" href="{{ route('logout') }}"
                                                onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
