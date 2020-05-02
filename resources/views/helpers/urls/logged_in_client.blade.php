0d6c04851a1e6a64eae70dcb153e927a85a561c8
Hayk Papyan <hayk803@gmail.com>
2020-05-02 23:13:22 +0400

    client home page

--- resources/views/helpers/urls/logged_in_client.blade.php
+++ resources/views/helpers/urls/logged_in_client.blade.php
@@ -1,4 +1,4 @@
-<li class="dropdown menu-item sign-hide"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="{{asset('images/user.png')}}" alt="">{{ Auth::user()->email }}<span class="caret"></span></a>
+<li class="dropdown menu-item sign-hide" ><a href="#" onclick="location.href='{{ url('/client/details') }}'" class="dropdown-toggle" data-toggle="dropdown"><img src="{{asset('images/user.png')}}" alt="">{{ Auth::user()->email }}<span class="caret"></span></a>
     <ul id="products-menu" class="dropdown-menu registration mr-0 ml-0" role="menu">
 
         <li class="menu-item sign-hide"><a href="{{ route('logout') }}"
