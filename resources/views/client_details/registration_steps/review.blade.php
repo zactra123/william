504574cc5642c84ce858aa595ade31c207ec1c6b
Hayk Papyan <hayk803@gmail.com>
2020-05-07 00:15:06 +0400

    test

--- resources/views/client_details/registration_steps/review.blade.php
+++ resources/views/client_details/registration_steps/review.blade.php
@@ -47,13 +47,13 @@
                 </div>
             </div>
         </div>
-        <div class="form-group row font justify-content-center">
-            <div class="col-md-12 tab-selector">
-                <div class="col-md-12">
-                    {{ Form::text('client[zip]',  !empty($uploadUserDetail) ? $uploadUserDetail->zip :  $client->clientDetails->ssn, ['class' => 'form-control m-input', 'id'=>'zip_code','placeholder' => 'ZIP CODE']) }}
-                </div>
-            </div>
-        </div>
+{{--        <div class="form-group row font justify-content-center">--}}
+{{--            <div class="col-md-12 tab-selector">--}}
+{{--                <div class="col-md-12">--}}
+{{--                    {{ Form::text('client[zip]',  !empty($uploadUserDetail) ? $uploadUserDetail->zip :  $client->clientDetails->ssn, ['class' => 'form-control m-input', 'id'=>'zip_code','placeholder' => 'ZIP CODE']) }}--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
         <div class="form-group row font justify-content-center">
             <div class="col-md-12 tab-selector">
                 <div class="col-md-12 sex_options">
@@ -91,6 +91,7 @@
         autocomplete = new google.maps.places.Autocomplete($("#address")[0], { types: ['address'], componentRestrictions: {country: "us"}});
         google.maps.event.addListener(autocomplete, 'place_changed', function() {
             var place = autocomplete.getPlace();
+            $("#address").val(place.formatted_address)
             for (var i = 0; i < place.address_components.length; i++) {
                 for (var j = 0; j < place.address_components[i].types.length; j++) {
                     if (place.address_components[i].types[j] == "postal_code") {
