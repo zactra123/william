8ceac742195028a87d4d7dce98734983eb43e965
AraratPapyan <papyanararat11@gmail.com>
2020-09-12 04:26:57 +0400

    change who we are and how it works

--- resources/views/how-it-works.blade.php
+++ resources/views/how-it-works.blade.php
@@ -26,14 +26,18 @@
                     <div class="col-md-7">
                         <div class="wrapper-content">
                             <p class="p-4"     style="text-align: justify">
-                                When you decide to hire our firm for your credit restoration needs, we direct you to register and create an
-                                account. Then, with your help and authorization, our proprietary software scans and conducts credit audit
-                                on your credit reports by searching for incomplete, unverifiable, misleading, frivolous, erroneous, obsolete,
-                                inaccurate, unauthorized or fraudulent information. Next, you review our findings and sign the credit
-                                restoration contract and simultaneously apply for a personal loan account with Exceed Capital Lending.
-                                Now, we go to work, and you sit back, relax, and expect to hear some good news. Lastly, we will notify you
-                                and Exceed Capital Lending once an item or items have been corrected/removed from your credit report for the
-                                payment disbursement for the completed work.
+
+                                When you decide to hire our firm for your credit restoration needs, we direct you to
+                                register and create an account. Then, with your help and authorization, our proprietary
+                                software scans and conducts credit audit on your credit reports by searching for
+                                incomplete, unverifiable, misleading, frivolous, erroneous, obsolete, inaccurate,
+                                unauthorized, or fraudulent information. Next, you review our findings, sign the credit
+                                restoration contract, and apply for a personal loan account with Exceed Capital Lending
+                                (optional). Now, we go to work, and you sit back, relax, and expect to hear some good
+                                news. Lastly, once credit reporting agencies update or remove a disputed item or items
+                                from your credit report, we will notify you and Exceed Capital Lending (given that you
+                                chose finance option) for payment disbursement.
+
 
                             </p>
 
