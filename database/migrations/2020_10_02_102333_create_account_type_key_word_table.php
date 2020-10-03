8ef062621dbd431318dc2e7ed070a93f3a240267
AraratPapyan <papyanararat11@gmail.com>
2020-10-03 18:08:06 +0400

    ondelete cascade

--- database/migrations/2020_10_02_102333_create_account_type_key_word_table.php
+++ database/migrations/2020_10_02_102333_create_account_type_key_word_table.php
@@ -16,9 +16,9 @@ class CreateAccountTypeKeyWordTable extends Migration
         Schema::create('account_type_key_word', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('account_type_key_id');
-            $table->foreign('account_type_key_id')->references('id')->on('account_type_keys');
+            $table->foreign('account_type_key_id')->references('id')->on('account_type_keys')->onDelete('cascade');
             $table->unsignedBigInteger('account_type_id');
-            $table->foreign('account_type_id')->references('id')->on('account_types');
+            $table->foreign('account_type_id')->references('id')->on('account_types')->onDelete('cascade');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2020_10_03_120239_change_structer_bank_addresses_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeStructerBankAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        Schema::table('bank_addresses', function (Blueprint $table) {
+
+//            $table->dropForeign('ba_bank_account_id');
+//            $table->dropColumn('bank_account_id');
+
+            $table->unsignedBigInteger('bank_logo_id')->after('id');
+            $table->foreign('bank_logo_id', 'bank_logo_id')->references('id')->on('bank_logos')->onDelete('cascade');
+            $table->unsignedBigInteger('account_type_id')->after('bank_logo_id');
+            $table->foreign('account_type_id', 'account_type_id')->references('id')->on('account_types')->onDelete('cascade')->nullable();
+
+        });
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        Schema::dropIfExists('bank_phone_numbers');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
