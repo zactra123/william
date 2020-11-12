dc4690ba6303c867853b81b4ce24e6c83273facd
Hayk Papyan <hayk803@gmail.com>
2020-11-12 23:09:57 +0400

    hide autocomplete

--- resources/views/todo/profile.blade.php
+++ resources/views/todo/profile.blade.php
@@ -1101,25 +1101,25 @@
     </script>
 
     <script>
-        $(document).ready(function() {
-
-            autocomplete = new google.maps.places.Autocomplete($("#address")[0], {
-                types: ['address'],
-                componentRestrictions: {country: "us"}
-            });
-            google.maps.event.addListener(autocomplete, 'place_changed', function () {
-                var place = autocomplete.getPlace();
-                $("#address").val(place.formatted_address)
-                for (var i = 0; i < place.address_components.length; i++) {
-                    for (var j = 0; j < place.address_components[i].types.length; j++) {
-                        if (place.address_components[i].types[j] == "postal_code") {
-                            $("#zip").val(place.address_components[i].long_name);
-
-                        }
-                    }
-                }
-            });
-        })
+        // $(document).ready(function() {
+        //
+        //     autocomplete = new google.maps.places.Autocomplete($("#address")[0], {
+        //         types: ['address'],
+        //         componentRestrictions: {country: "us"}
+        //     });
+        //     google.maps.event.addListener(autocomplete, 'place_changed', function () {
+        //         var place = autocomplete.getPlace();
+        //         $("#address").val(place.formatted_address)
+        //         for (var i = 0; i < place.address_components.length; i++) {
+        //             for (var j = 0; j < place.address_components[i].types.length; j++) {
+        //                 if (place.address_components[i].types[j] == "postal_code") {
+        //                     $("#zip").val(place.address_components[i].long_name);
+        //
+        //                 }
+        //             }
+        //         }
+        //     });
+        // })
     </script>
     <script>
         $(document).ready(function() {
