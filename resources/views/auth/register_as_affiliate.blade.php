4048c311cd569b3d1340e40d5fd9f4485e134622
Hayk Papyan <hayk803@gmail.com>
2020-12-04 01:28:57 +0400

    commetn autocomplete address

--- resources/views/auth/register_as_affiliate.blade.php
+++ resources/views/auth/register_as_affiliate.blade.php
@@ -210,25 +210,23 @@
             </ul>
         </div>
     </script>
-    <script   src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBSYolQg54i3oiTNu7T3pA2plmtS6Pshwg&libraries=places">
+{{--    <script   src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBSYolQg54i3oiTNu7T3pA2plmtS6Pshwg&libraries=places"></script>--}}
+{{--    <script>--}}
+{{--        autocomplete = new google.maps.places.Autocomplete($("#address")[0], { types: ['address'], componentRestrictions: {country: "us"}});--}}
+{{--        google.maps.event.addListener(autocomplete, 'place_changed', function() {--}}
+{{--            var place = autocomplete.getPlace();--}}
+{{--            $("#address").val(place.formatted_address)--}}
+{{--            for (var i = 0; i < place.address_components.length; i++) {--}}
+{{--                for (var j = 0; j < place.address_components[i].types.length; j++) {--}}
+{{--                    if (place.address_components[i].types[j] == "postal_code") {--}}
+{{--                        $("#zip").val(place.address_components[i].long_name);--}}
 
-    </script>
-    <script>
-        autocomplete = new google.maps.places.Autocomplete($("#address")[0], { types: ['address'], componentRestrictions: {country: "us"}});
-        google.maps.event.addListener(autocomplete, 'place_changed', function() {
-            var place = autocomplete.getPlace();
-            $("#address").val(place.formatted_address)
-            for (var i = 0; i < place.address_components.length; i++) {
-                for (var j = 0; j < place.address_components[i].types.length; j++) {
-                    if (place.address_components[i].types[j] == "postal_code") {
-                        $("#zip").val(place.address_components[i].long_name);
+{{--                    }--}}
+{{--                }--}}
+{{--            }--}}
+{{--        });--}}
 
-                    }
-                }
-            }
-        });
-
-    </script>
+{{--    </script>--}}
 
 
     <script src="{{ asset('js/lib/jquery.validate.min.js?v=2') }}" defer></script>
