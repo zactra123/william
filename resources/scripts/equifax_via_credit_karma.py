ac1792e95a2e02a0c461549df6488b088d13705d
Hayk Papyan <hayk803@gmail.com>
2020-12-09 22:59:04 +0400

    fix

--- resources/scripts/equifax_via_credit_karma.py
+++ resources/scripts/equifax_via_credit_karma.py
@@ -84,7 +84,6 @@ class equifaxCreditKarma:
         self.driver.get('https://www.creditkarma.com/credit-health/equifax/credit-report')
         WebDriverWait(self.driver, 300).until(EC.url_to_be('https://www.creditkarma.com/credit-health/equifax/credit-report'))
         time.sleep(10)
-        print("trying to access report")
         for request in self.driver.requests:
             self.process_browser_log_entry(request)
         time.sleep(10)
@@ -99,7 +98,6 @@ class equifaxCreditKarma:
                             sorted = json.dumps(body, indent=4)
                             f.write(sorted)
         except Exception as e:
-            print(e)
             pass
 
 
