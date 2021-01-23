0276da955c6271266fdc6843f0966af1858aa828
Hayk Papyan <hayk803@gmail.com>
2021-01-23 21:45:43 +0400

    fix canonical

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,12 +5,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
     <meta name="keywords" content="">
     <meta name="author" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="canonical" href="{{ url()->current() }}" />
 
-    <?php dd(Request::url() )?>
     @if(Request::url() == "https://prudentscores.com")
         <title>Superior credit restoration firm - Prudent Credit Solutions </title>
         <meta name="description" content="We resolve inaccuracies with - Bankruptcy, Mortgage Negatives, Late Payment
