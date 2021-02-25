7adbd72769a9e81c4302238fe628cd7954cc7313
Hayk Papyan <hayk803@gmail.com>
2021-02-26 00:18:09 +0400

    no follow on social login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -52,11 +52,11 @@
             </form>
 
             <div class="login-social">
-                <a class="login-facebook" href="{{route('facebook.login')}}" target="_blank" title="facebook">
+                <a class="login-facebook" href="{{route('facebook.login')}}" target="_blank" title="facebook" rel="nofollow">
                     <svg id="Bold" fill="#FFFFFF" enable-background="new 0 0 24 24" viewBox="0 0 24 24" width="512" xmlns="http://www.w3.org/2000/svg"><path d="m15.997 3.985h2.191v-3.816c-.378-.052-1.678-.169-3.192-.169-3.159 0-5.323 1.987-5.323 5.639v3.361h-3.486v4.266h3.486v10.734h4.274v-10.733h3.345l.531-4.266h-3.877v-2.939c.001-1.233.333-2.077 2.051-2.077z"/></svg>
                     Log in with Facebook
                 </a>
-                <a class="login-google" href="{{route('google.login')}}" target="_blank" title="google">
+                <a class="login-google" href="{{route('google.login')}}" target="_blank" title="google" rel="nofollow">
                     <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                          viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
 					<path style="fill:#FBBB00;" d="M113.47,309.408L95.648,375.94l-65.139,1.378C11.042,341.211,0,299.9,0,256
